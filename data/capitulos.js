5d2f4d78a1b9c39528480c2e6be7e9a2bc9fa984
Yugiidame20 <supercalistenia145@gmail.com>
2026-05-12 07:35:43 -0300

    update capitulos.js

--- data/capitulos.js
+++ data/capitulos.js
@@ -19,7 +19,7 @@ const capitulos = [
 
             titulo:"",
 
-            imagem:"../Home_Imgs/cap1.webp"
+            imagem:"Home_Imgs/cap1.webp"
         },
 
         {
@@ -163,7 +163,7 @@ const capitulos = [
             tipo:"imagem",
 
             imagem:
-            "../assets/ilustracoes/cen1-cap1.webp"
+            "assets/ilustracoes/cen1-cap1.webp"
         },
         
         {
@@ -337,7 +337,7 @@ const capitulos = [
             tipo:"imagem",
 
             imagem:
-            "../assets/ilustracoes/cen2-cap1.webp"
+            "assets/ilustracoes/cen2-cap1.webp"
         },
         
         {
@@ -441,7 +441,7 @@ const capitulos = [
             tipo:"imagem",
 
             imagem:
-            "../assets/ilustracoes/cen3-cap1.webp"
+            "assets/ilustracoes/cen3-cap1.webp"
         },
         
         {
@@ -511,7 +511,7 @@ const capitulos = [
             tipo:"imagem",
 
             imagem:
-            "../assets/ilustracoes/cen4-cap1.webp"
+            "assets/ilustracoes/cen4-cap1.webp"
         },
         
         {
@@ -620,7 +620,7 @@ const capitulos = [
             tipo:"imagem",
 
             imagem:
-            "../assets/ilustracoes/cen5-cap1.webp"
+            "assets/ilustracoes/cen5-cap1.webp"
         },
         
         {
@@ -763,7 +763,7 @@ const capitulos = [
             tipo:"imagem",
 
             imagem:
-            "../assets/ilustracoes/cen6-cap1.webp"
+            "assets/ilustracoes/cen6-cap1.webp"
         },
         
         {
@@ -818,7 +818,7 @@ const capitulos = [
             tipo:"imagem",
 
             imagem:
-            "../assets/ilustracoes/cen7-cap1.webp"
+            "assets/ilustracoes/cen7-cap1.webp"
         },
 
 
@@ -844,7 +844,7 @@ const capitulos = [
 
             titulo:"TEMPESTADE",
 
-            imagem:"../Home_Imgs/cap2.jpg"
+            imagem:"Home_Imgs/cap2.jpg"
         },
 
         {
